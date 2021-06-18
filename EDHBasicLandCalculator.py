4705b1f48bbe21ff86a6ee473480e37a798969b4
Jamesgarrett279 <jamesgarrett279@gmail.com>
2021-06-17 22:32:25 -0400

    Changed the name of the .py file.

--- EDHBasicLandCalculator.py
+++ EDHBasicLandCalculator.py
@@ -1,5 +1,8 @@
 # Author: James Carter Garrett
 
+# This is the main file for the mean time. However, as this project progresses, there will be calculators for different formats (Modern, Draft, etc.)
+
+
 import json
 import requests
 import sys
@@ -29,7 +32,7 @@ def decklistReader():
     cardCount, symbolCount = 0, 0
 
     if (fileChecker() == False):
-        sys.exit()
+        return
 
     with open(sys.argv[1], "r", encoding="utf-8") as ourFile:
         fileContents = ourFile.read()
@@ -55,14 +58,15 @@ def decklistReader():
                 print(api.status_code)
             
             if (api.status_code == 404):
-                sys.exit("Error: Card not found")
+                print("Error: Card not found")
+                return
 
             apiRequest = api.json()
             time.sleep(.05)
 
         except Exception as ex:
             print(ex)
-            sys.exit("There has been an error")
+            return
 
         # Checks to see if the card is a land / has no mana cost and goes to next card if it is
         if (apiRequest["mana_cost"] == ''):
@@ -203,7 +207,7 @@ def decklistReader():
                     # If the symbol is not one of the above cases, it's either not a symbol or it's from an un-set
                     else:
                         print("A non-valid symbol has been detected. Please make sure all cards in your list are black-bordered cards.")
-                        sys.exit()
+                        return
 
     # Calculations
     basicLands = (100 - cardCount) # 100 can be changed depending on the format
@@ -217,7 +221,7 @@ def decklistReader():
 
     else:
         print("There are no cards with mana costs in the list, so the conversion will not run.")
-        sys.exit()
+        return
 
     # Print out the information (this will eventually be in a gui)
     print("Your total basic lands count is: " + str(basicLands))
@@ -228,9 +232,14 @@ def decklistReader():
     print("The number of mountains you should have is: " + mountains + " || Symbol Count: " + str(rSymbol))
     print("The number of forests you should have is: " + forests + " || Symbol Count: " + str(gSymbol))
 
-
 def main():
     decklistReader()
 
 if __name__ == "__main__":
+    print("Running program...")
+    print()
+
     main()
+
+    print()
+    input("Press enter to end the program.")
